7300f34b4017103ac47007965ba3904d6d8a5085
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2024-05-06 02:13:28 +0300

    v11.6

--- src/pages/QuotePage.jsx
+++ src/pages/QuotePage.jsx
@@ -16,12 +16,12 @@ const PriceLiTags = ({ content }) => {
 const QuotePage = () => {
   return (
     <div className="h-auto w-full bg-[#ededed]">
-      <main className="flex f-center mt-16 mb-16 pt-16 h-[85vh]">
+      <main className="flex f-center mt-16 mb-16 pt-16 h-[85vh] tablet:h-auto tablet:w-full">
         {/*Main Section*/}
-        <section className="grid grid-cols-2 grid-rows-2 w-4/6 h-full">
+        <section className="grid min-tablet:grid-cols-2 grid-rows-2 tablet:grid-rows-[1fr_400px_420px_300px] w-4/6 h-full tablet:w-10/12 tablet:gap-4">
           {/*Price Section*/}
-          <section className="flex gap-4">
-            <div className="h-[90%] w-5/12 bg-green-400 rounded-lg flex flex-col items-center justify-evenly">
+          <section className="flex gap-4 tablet:flex-col tablet:w-full">
+            <div className="h-[90%] w-5/12 bg-green-400 rounded-lg flex flex-col items-center justify-evenly tablet:w-full tablet:h-80">
               <h2 className="text-white text-center h-6">Best Dial</h2>
               <div className="bg-white w-[98%] h-[86%] rounded-md flex flex-col f-center">
                 <div className="w-10/12 flex flex-col gap-2">
@@ -46,7 +46,7 @@ const QuotePage = () => {
                 </div>
               </div>
             </div>
-            <div className="h-[90%] w-5/12 bg-white rounded-lg flex flex-col items-center justify-evenly">
+            <div className="h-[90%] w-5/12 bg-white rounded-lg flex flex-col items-center justify-evenly tablet:w-full tablet:h-80">
               <h2 className="text-white text-center h-6"></h2>
               <div className="bg-white w-[98%] h-[86%] rounded-md flex flex-col f-center">
                 <div className="w-10/12 flex flex-col gap-2">
@@ -68,11 +68,11 @@ const QuotePage = () => {
           {/*Map Section*/}
           <section>
             <h2>Your Trip</h2>
-            <div className="h-5/6 w-full bg-white shadow-xl rounded-sm"></div>
+            <div className="h-5/6 w-full bg-white shadow-xl rounded-sm tablet:h-5/6 tablet:mt-4"></div>
           </section>
           {/*Map Section*/}
           {/*Information Section*/}
-          <section className="bg-white rounded-lg h-full w-[97%] flex f-center">
+          <section className="bg-white rounded-lg h-full w-[97%] flex f-center tablet:justify-self-center">
             <div className="w-[87%] h-full mt-4">
               <h1 className="font-bold mb-4 text-2xl">Summary</h1>
               <div className="w-full h-20 border-[1px] border-solid border-slate-400 rounded-lg flex flex-col ft-center">
@@ -101,8 +101,8 @@ const QuotePage = () => {
           </section>
           {/*Information Section*/}
           {/*Continue Section*/}
-          <section className="bg-white rounded-lg w-full h-4/6 flex f-center">
-            <div className="h-5/6 w-11/12">
+          <section className="bg-white rounded-lg w-full h-4/6 tablet:h-full flex f-center tablet:self-center">
+            <div className="h-5/6 w-11/12 tablet:h-full tablet:flex tablet:flex-col tablet:gap-2 tablet:mt-8">
               <div className="flex gap-3 items-center justify-start">
                 <p>Due Now</p>
                 <p className="text-green-400 text-5xl">$0</p>
@@ -115,13 +115,13 @@ const QuotePage = () => {
                 <input
                   type="text"
                   placeholder="Ex. SAVE20"
-                  className="outline-none rounded my-3 py-2 px-2 w-5/12 border-[1px] border-solid border-slate-200"
+                  className="outline-none rounded my-3 py-2 px-2 w-5/12 border-[1px] border-solid border-slate-200 tablet:basis-1/2"
                 />
-                <button className="text-white bg-green-400 my-3 px-3 rounded-lg font-normal">
+                <button className="text-white bg-green-400 my-3 px-3 rounded-lg font-normal tablet:basis-1/2">
                   Apply Code
                 </button>
               </div>
-              <button className="text-white bg-green-400 px-3 py-2 rounded-lg font-normal w-5/12">
+              <button className="text-white bg-green-400 px-3 py-2 rounded-lg font-normal w-5/12 tablet:w-full">
                 Continue to booking details
               </button>
             </div>
